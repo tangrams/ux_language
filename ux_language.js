7bcf0287e32e6f0acf9488090b28c828856f8b2e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-10-06 17:11:04 -0400

    adding icon

--- ux_language.js
+++ ux_language.js
@@ -25,24 +25,35 @@ L.UxLanguage = L.Control.extend({
 
     onAdd: function(map) {
         var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom ux_language-container');
-        var UxLanguage = L.DomUtil.create('SELECT', 'ux_language-selector', container);
+        var image =  L.DomUtil.create('img', 'ux_language-icon', container);
+        image.src = 'https://tangrams.github.io/ux_language/ux_language.png';
+
+        var selector = L.DomUtil.create('SELECT', 'ux_language-selector', container);
+        selector.style.backgroundColor = "rgba(0, 0, 0, 0)"
+        
         for (var key in this.options.languages) {
             var language = L.DomUtil.create('option', 'ux_language-option');
             if (!this.options.languages[key]) {
-                language.text = '-- - (Default)';
+                language.text = '';
             }
             else {
                 language.text = this.options.languages[key] + ' - ' + key;
             }
             language.value = this.options.languages[key];
-            UxLanguage.add(language);
+            selector.add(language);
         }
         var options = this.options;
-        UxLanguage.selectedIndex = 0;
-        UxLanguage.onchange = function(onchange){
+        selector.selectedIndex = 0;
+        selector.onchange = function(onchange){
             console.log('Changing language for', onchange.target.value);
             options.scene.config.global[options.global] = onchange.target.value;
             options.scene.updateConfig();
+
+            if (onchange.target.value == 'false') {
+                selector.style.backgroundColor = "rgba(0, 0, 0, 0)"
+            } else {
+                selector.style.backgroundColor = '#FFF';
+            }
         }
 
         return container;
